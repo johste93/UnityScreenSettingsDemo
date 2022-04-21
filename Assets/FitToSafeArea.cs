d6de28acc7dae5f8a6de2125c34cec0dd4196605
Johannes Stensen <pm@johste.com>
2022-04-21 22:49:48 +0200

    Some improvements.

---
+++ Assets/FitToSafeArea.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FitToSafeArea : MonoBehaviour
+{
+    public RectTransform rectTransform;
+
+    private void Awake()
+    {
+        rectTransform = (RectTransform) transform;
+        rectTransform.anchorMin = rectTransform.anchorMax = rectTransform.pivot = Vector2.up;
+
+    }
+    
+    void Update()
+    {
+        Rect safeArea = Screen.safeArea;
+        rectTransform.anchoredPosition = safeArea.position;
+        rectTransform.sizeDelta = safeArea.size;
+    }
+}
--- Assets/Resolutions.cs
+++ Assets/Resolutions.cs
@@ -30,7 +30,13 @@ public class Resolutions : MonoBehaviour
         PlayerPrefs.DeleteAll();
         
         Event.options = Enum.GetNames(typeof(UnityEventFunction)).ToList().Select(x => new TMP_Dropdown.OptionData(x)).ToList();
+        Event.value = Event.options.Count - 1;
         FullScreenMode.options = Enum.GetNames(typeof(FullScreenMode)).ToList().Select(x => new TMP_Dropdown.OptionData(x)).ToList();
+
+        Width.text = Screen.currentResolution.width.ToString();
+        Height.text = Screen.currentResolution.height.ToString();
+        RefreshRate.text = Screen.currentResolution.refreshRate.ToString();
+        FullScreenMode.value = (int) Screen.fullScreenMode;
     }
     
     public void UpdateSettings()
@@ -38,8 +44,7 @@ public class Resolutions : MonoBehaviour
         if (int.TryParse(Width.text, out int width) && int.TryParse(Height.text, out int height) && int.TryParse(RefreshRate.text, out int refreshRate))
         {
             FullScreenMode fullScreenMode = (FullScreenMode) FullScreenMode.value;
-            Debug.Log($"{width}, {height}, {fullScreenMode}, {refreshRate}");
-            //Screen.SetResolution(width, height, fullScreenMode, refreshRate);
+            Screen.SetResolution(width, height, fullScreenMode, refreshRate);
         }
         else
         {
@@ -102,66 +107,71 @@ public class Resolutions : MonoBehaviour
     private void Awake()
     {
         InitalizeSettings();
-        UnityEngineScreenFullScreenAwake.text = $"UnityEngine.Screen.fullScreen: {UnityEngine.Screen.fullScreen}";
-        UnityEngineScreenFullScreenModeAwake.text = $"UnityEngine.Screen.fullScreenMode: {UnityEngine.Screen.fullScreenMode}";
-        UnityEngineApplicationTargetFrameRateAwake.text = $"UnityEngine.Application.targetFrameRate: {UnityEngine.Application.targetFrameRate}";
-        UnityEngineQualtiySettingsVSyncCountAwake.text = $"UnityEngine.QualitySettings.vSyncCount: {UnityEngine.QualitySettings.vSyncCount}";
-        UnityEngineScreenWidthHeightAwake.text = $"UnityEngine.Screen.width/height: {UnityEngine.Screen.width}x{UnityEngine.Screen.height}";
-        UnityEngineDeviceScreenWidthHeightAwake.text = $"UnityEngine.Device.Screen.width/height/refreshRate: {UnityEngine.Device.Screen.width}x{UnityEngine.Device.Screen.height} ({UnityEngine.Screen.currentResolution.refreshRate})";
-        UnityEngineScreenCurrentResolutionWidthHeightAwake.text = $"UnityEngine.Screen.currentResolution.width/height: {UnityEngine.Screen.currentResolution.width}x{UnityEngine.Screen.currentResolution.height}";
-        UnityEngineDisplayMainSystemWidthHeightAwake.text = $"UnityEngine.Display.main.systemWidth/systemHeight: {UnityEngine.Display.main.systemWidth}x{UnityEngine.Display.main.systemHeight}";
-        UnityEngineDisplayRenderingWidthHeightAwake.text = $"UnityEngine.Display.main.renderingWidth/renderingHeight: {UnityEngine.Display.main.renderingWidth}x{UnityEngine.Display.main.renderingHeight}";
-        UnityEngineScreenResolutionsAwake.text = "UnityEngine.Screen.resolutions: " + string.Join(",", UnityEngine.Screen.resolutions.ToList().Select(x => $"{x.width}x{x.height} ({x.refreshRate})").ToList());
-        UnityEngineScreenSafeAreaAwake.text = $"UnityEngine.Screen.safeArea: {UnityEngine.Screen.safeArea.ToString()}";
+        UnityEngineScreenFullScreenAwake.text = $"UnityEngine.Screen.fullScreen: <b>{UnityEngine.Screen.fullScreen}</b>";
+        UnityEngineScreenFullScreenModeAwake.text = $"UnityEngine.Screen.fullScreenMode: <b>{UnityEngine.Screen.fullScreenMode}</b>";
+        UnityEngineApplicationTargetFrameRateAwake.text = $"UnityEngine.Application.targetFrameRate: <b>{UnityEngine.Application.targetFrameRate}</b>";
+        UnityEngineQualtiySettingsVSyncCountAwake.text = $"UnityEngine.QualitySettings.vSyncCount: <b>{UnityEngine.QualitySettings.vSyncCount}</b>";
+        UnityEngineScreenWidthHeightAwake.text = $"UnityEngine.Screen.width/height: <b>{UnityEngine.Screen.width}x{UnityEngine.Screen.height}</b>";
+        UnityEngineDeviceScreenWidthHeightAwake.text = $"UnityEngine.Device.Screen.width/height/refreshRate: <b>{UnityEngine.Device.Screen.width}x{UnityEngine.Device.Screen.height} ({UnityEngine.Screen.currentResolution.refreshRate})</b>";
+        UnityEngineScreenCurrentResolutionWidthHeightAwake.text = $"UnityEngine.Screen.currentResolution.width/height: <b>{UnityEngine.Screen.currentResolution.width}x{UnityEngine.Screen.currentResolution.height}</b>";
+        UnityEngineDisplayMainSystemWidthHeightAwake.text = $"UnityEngine.Display.main.systemWidth/systemHeight: <b>{UnityEngine.Display.main.systemWidth}x{UnityEngine.Display.main.systemHeight}</b>";
+        UnityEngineDisplayRenderingWidthHeightAwake.text = $"UnityEngine.Display.main.renderingWidth/renderingHeight: <b>{UnityEngine.Display.main.renderingWidth}x{UnityEngine.Display.main.renderingHeight}</b>";
+        UnityEngineScreenResolutionsAwake.text = $"UnityEngine.Screen.resolutions:\n<b>{string.Join(",", UnityEngine.Screen.resolutions.ToList().Select(x => $"{x.width}x{x.height} ({x.refreshRate})").ToList())}</b>";
+        UnityEngineScreenSafeAreaAwake.text = $"UnityEngine.Screen.safeArea: <b>{UnityEngine.Screen.safeArea.ToString()}</b>";
     }
     
     private void Start()
     {
-        UnityEngineScreenFullScreenStart.text = $"UnityEngine.Screen.fullScreen: {UnityEngine.Screen.fullScreen}";
-        UnityEngineScreenFullScreenModeStart.text = $"UnityEngine.Screen.fullScreenMode: {UnityEngine.Screen.fullScreenMode}";
-        UnityEngineApplicationTargetFrameRateStart.text = $"UnityEngine.Application.targetFrameRate: {UnityEngine.Application.targetFrameRate}";
-        UnityEngineQualtiySettingsVSyncCountStart.text = $"UnityEngine.QualitySettings.vSyncCount: {UnityEngine.QualitySettings.vSyncCount}";
-        UnityEngineScreenWidthHeightStart.text = $"UnityEngine.Screen.width/height: {UnityEngine.Screen.width}x{UnityEngine.Screen.height}";
-        UnityEngineDeviceScreenWidthHeightStart.text = $"UnityEngine.Device.Screen.width/height/refreshRate: {UnityEngine.Device.Screen.width}x{UnityEngine.Device.Screen.height} ({UnityEngine.Screen.currentResolution.refreshRate})";
-        UnityEngineScreenCurrentResolutionWidthHeightStart.text = $"UnityEngine.Screen.currentResolution.width/height: {UnityEngine.Screen.currentResolution.width}x{UnityEngine.Screen.currentResolution.height}";
-        UnityEngineDisplayMainSystemWidthHeightStart.text = $"UnityEngine.Display.main.systemWidth/systemHeight: {UnityEngine.Display.main.systemWidth}x{UnityEngine.Display.main.systemHeight}";
-        UnityEngineDisplayRenderingWidthHeightStart.text = $"UnityEngine.Display.main.renderingWidth/renderingHeight: {UnityEngine.Display.main.renderingWidth}x{UnityEngine.Display.main.renderingHeight}";
-        UnityEngineScreenResolutionsStart.text = "UnityEngine.Screen.resolutions: " +  string.Join(",", UnityEngine.Screen.resolutions.ToList().Select(x => $"{x.width}x{x.height} ({x.refreshRate})").ToList());
-        UnityEngineScreenSafeAreaStart.text = $"UnityEngine.Screen.safeArea: {UnityEngine.Screen.safeArea.ToString()}";
+        UnityEngineScreenFullScreenStart.text = $"UnityEngine.Screen.fullScreen: <b>{UnityEngine.Screen.fullScreen}</b>";
+        UnityEngineScreenFullScreenModeStart.text = $"UnityEngine.Screen.fullScreenMode: <b>{UnityEngine.Screen.fullScreenMode}</b>";
+        UnityEngineApplicationTargetFrameRateStart.text = $"UnityEngine.Application.targetFrameRate: <b>{UnityEngine.Application.targetFrameRate}</b>";
+        UnityEngineQualtiySettingsVSyncCountStart.text = $"UnityEngine.QualitySettings.vSyncCount: <b>{UnityEngine.QualitySettings.vSyncCount}</b>";
+        UnityEngineScreenWidthHeightStart.text = $"UnityEngine.Screen.width/height: <b>{UnityEngine.Screen.width}x{UnityEngine.Screen.height}</b>";
+        UnityEngineDeviceScreenWidthHeightStart.text = $"UnityEngine.Device.Screen.width/height/refreshRate: <b>{UnityEngine.Device.Screen.width}x{UnityEngine.Device.Screen.height} ({UnityEngine.Screen.currentResolution.refreshRate})</b>";
+        UnityEngineScreenCurrentResolutionWidthHeightStart.text = $"UnityEngine.Screen.currentResolution.width/height: <b>{UnityEngine.Screen.currentResolution.width}x{UnityEngine.Screen.currentResolution.height}</b>";
+        UnityEngineDisplayMainSystemWidthHeightStart.text = $"UnityEngine.Display.main.systemWidth/systemHeight: <b>{UnityEngine.Display.main.systemWidth}x{UnityEngine.Display.main.systemHeight}</b>";
+        UnityEngineDisplayRenderingWidthHeightStart.text = $"UnityEngine.Display.main.renderingWidth/renderingHeight: <b>{UnityEngine.Display.main.renderingWidth}x{UnityEngine.Display.main.renderingHeight}</b>";
+        UnityEngineScreenResolutionsStart.text = $"UnityEngine.Screen.resolutions:\n<b>{string.Join(",", UnityEngine.Screen.resolutions.ToList().Select(x => $"{x.width}x{x.height} ({x.refreshRate})").ToList())}</b>";
+        UnityEngineScreenSafeAreaStart.text = $"UnityEngine.Screen.safeArea: <b>{UnityEngine.Screen.safeArea.ToString()}</b>";
     }
     
     private void OnEnable()
     {
-        UnityEngineScreenFullScreenOnEnable.text = $"UnityEngine.Screen.fullScreen: {UnityEngine.Screen.fullScreen}";
-        UnityEngineScreenFullScreenModeOnEnable.text = $"UnityEngine.Screen.fullScreenMode: {UnityEngine.Screen.fullScreenMode}";
-        UnityEngineApplicationTargetFrameRateOnEnable.text = $"UnityEngine.Application.targetFrameRate: {UnityEngine.Application.targetFrameRate}";
-        UnityEngineQualtiySettingsVSyncCountOnEnable.text = $"UnityEngine.QualitySettings.vSyncCount: {UnityEngine.QualitySettings.vSyncCount}";
-        UnityEngineScreenWidthHeightOnEnable.text = $"UnityEngine.Screen.width/height: {UnityEngine.Screen.width}x{UnityEngine.Screen.height}";
-        UnityEngineDeviceScreenWidthHeightOnEnable.text = $"UnityEngine.Device.Screen.width/height/refreshRate: {UnityEngine.Device.Screen.width}x{UnityEngine.Device.Screen.height} ({UnityEngine.Screen.currentResolution.refreshRate})";
-        UnityEngineScreenCurrentResolutionWidthHeightOnEnable.text = $"UnityEngine.Screen.currentResolution.width/height: {UnityEngine.Screen.currentResolution.width}x{UnityEngine.Screen.currentResolution.height}";
-        UnityEngineDisplayMainSystemWidthHeightOnEnable.text = $"UnityEngine.Display.main.systemWidth/systemHeight: {UnityEngine.Display.main.systemWidth}x{UnityEngine.Display.main.systemHeight}";
-        UnityEngineDisplayRenderingWidthHeightOnEnable.text = $"UnityEngine.Display.main.renderingWidth/renderingHeight: {UnityEngine.Display.main.renderingWidth}x{UnityEngine.Display.main.renderingHeight}";
-        UnityEngineScreenResolutionsOnEnable.text = "UnityEngine.Screen.resolutions: " +  string.Join(",", UnityEngine.Screen.resolutions.ToList().Select(x => $"{x.width}x{x.height} ({x.refreshRate})").ToList());
-        UnityEngineScreenSafeAreaOnEnable.text = $"UnityEngine.Screen.safeArea: {UnityEngine.Screen.safeArea.ToString()}";
+        UnityEngineScreenFullScreenOnEnable.text = $"UnityEngine.Screen.fullScreen: <b>{UnityEngine.Screen.fullScreen}</b>";
+        UnityEngineScreenFullScreenModeOnEnable.text = $"UnityEngine.Screen.fullScreenMode: <b>{UnityEngine.Screen.fullScreenMode}</b>";
+        UnityEngineApplicationTargetFrameRateOnEnable.text = $"UnityEngine.Application.targetFrameRate: <b>{UnityEngine.Application.targetFrameRate}</b>";
+        UnityEngineQualtiySettingsVSyncCountOnEnable.text = $"UnityEngine.QualitySettings.vSyncCount: <b>{UnityEngine.QualitySettings.vSyncCount}</b>";
+        UnityEngineScreenWidthHeightOnEnable.text = $"UnityEngine.Screen.width/height: <b>{UnityEngine.Screen.width}x{UnityEngine.Screen.height}</b>";
+        UnityEngineDeviceScreenWidthHeightOnEnable.text = $"UnityEngine.Device.Screen.width/height/refreshRate: <b>{UnityEngine.Device.Screen.width}x{UnityEngine.Device.Screen.height} ({UnityEngine.Screen.currentResolution.refreshRate})</b>";
+        UnityEngineScreenCurrentResolutionWidthHeightOnEnable.text = $"UnityEngine.Screen.currentResolution.width/height: <b>{UnityEngine.Screen.currentResolution.width}x{UnityEngine.Screen.currentResolution.height}</b>";
+        UnityEngineDisplayMainSystemWidthHeightOnEnable.text = $"UnityEngine.Display.main.systemWidth/systemHeight: <b>{UnityEngine.Display.main.systemWidth}x{UnityEngine.Display.main.systemHeight}</b>";
+        UnityEngineDisplayRenderingWidthHeightOnEnable.text = $"UnityEngine.Display.main.renderingWidth/renderingHeight: <b>{UnityEngine.Display.main.renderingWidth}x{UnityEngine.Display.main.renderingHeight}</b>";
+        UnityEngineScreenResolutionsOnEnable.text = $"UnityEngine.Screen.resolutions:\n<b>{string.Join(",", UnityEngine.Screen.resolutions.ToList().Select(x => $"{x.width}x{x.height} ({x.refreshRate})").ToList())}</b>";
+        UnityEngineScreenSafeAreaOnEnable.text = $"UnityEngine.Screen.safeArea: <b>{UnityEngine.Screen.safeArea.ToString()}</b>";
     }
     
     private void Update()
     {
-        UnityEngineScreenFullScreen.text = $"UnityEngine.Screen.fullScreen: {UnityEngine.Screen.fullScreen}";
-        UnityEngineScreenFullScreenMode.text = $"UnityEngine.Screen.fullScreenMode: {UnityEngine.Screen.fullScreenMode}";
-        UnityEngineApplicationTargetFrameRate.text = $"UnityEngine.Application.targetFrameRate: {UnityEngine.Application.targetFrameRate}";
-        UnityEngineQualtiySettingsVSyncCount.text = $"UnityEngine.QualitySettings.vSyncCount: {UnityEngine.QualitySettings.vSyncCount}";
-        UnityEngineScreenWidthHeight.text = $"UnityEngine.Screen.width/height: {UnityEngine.Screen.width}x{UnityEngine.Screen.height}";
-        UnityEngineDeviceScreenWidthHeight.text = $"UnityEngine.Device.Screen.width/height/refreshRate: {UnityEngine.Device.Screen.width}x{UnityEngine.Device.Screen.height} ({UnityEngine.Screen.currentResolution.refreshRate})";
-        UnityEngineScreenCurrentResolutionWidthHeight.text = $"UnityEngine.Screen.currentResolution.width/height: {UnityEngine.Screen.currentResolution.width}x{UnityEngine.Screen.currentResolution.height}";
-        UnityEngineDisplayMainSystemWidthHeight.text = $"UnityEngine.Display.main.systemWidth/systemHeight: {UnityEngine.Display.main.systemWidth}x{UnityEngine.Display.main.systemHeight}";
-        UnityEngineDisplayRenderingWidthHeight.text = $"UnityEngine.Display.main.renderingWidth/renderingHeight: {UnityEngine.Display.main.renderingWidth}x{UnityEngine.Display.main.renderingHeight}";
-        UnityEngineScreenResolutions.text = "UnityEngine.Screen.resolutions: " + string.Join(",", UnityEngine.Screen.resolutions.ToList().Select(x => $"{x.width}x{x.height} ({x.refreshRate})").ToList());
-        UnityEngineScreenSafeArea.text = $"UnityEngine.Screen.safeArea: {UnityEngine.Screen.safeArea.ToString()}";
+        UnityEngineScreenFullScreen.text = $"UnityEngine.Screen.fullScreen: <b>{UnityEngine.Screen.fullScreen}</b>";
+        UnityEngineScreenFullScreenMode.text = $"UnityEngine.Screen.fullScreenMode: <b>{UnityEngine.Screen.fullScreenMode}</b>";
+        UnityEngineApplicationTargetFrameRate.text = $"UnityEngine.Application.targetFrameRate: <b>{UnityEngine.Application.targetFrameRate}</b>";
+        UnityEngineQualtiySettingsVSyncCount.text = $"UnityEngine.QualitySettings.vSyncCount: <b>{UnityEngine.QualitySettings.vSyncCount}</b>";
+        UnityEngineScreenWidthHeight.text = $"UnityEngine.Screen.width/height: <b>{UnityEngine.Screen.width}x{UnityEngine.Screen.height}</b>";
+        UnityEngineDeviceScreenWidthHeight.text = $"UnityEngine.Device.Screen.width/height/refreshRate: <b>{UnityEngine.Device.Screen.width}x{UnityEngine.Device.Screen.height} ({UnityEngine.Screen.currentResolution.refreshRate})</b>";
+        UnityEngineScreenCurrentResolutionWidthHeight.text = $"UnityEngine.Screen.currentResolution.width/height: <b>{UnityEngine.Screen.currentResolution.width}x{UnityEngine.Screen.currentResolution.height}</b>";
+        UnityEngineDisplayMainSystemWidthHeight.text = $"UnityEngine.Display.main.systemWidth/systemHeight: <b>{UnityEngine.Display.main.systemWidth}x{UnityEngine.Display.main.systemHeight}</b>";
+        UnityEngineDisplayRenderingWidthHeight.text = $"UnityEngine.Display.main.renderingWidth/renderingHeight: <b>{UnityEngine.Display.main.renderingWidth}x{UnityEngine.Display.main.renderingHeight}</b>";
+        UnityEngineScreenResolutions.text = $"UnityEngine.Screen.resolutions:\n<b>{string.Join(",", UnityEngine.Screen.resolutions.ToList().Select(x => $"{x.width}x{x.height} ({x.refreshRate})").ToList())}</b>";
+        UnityEngineScreenSafeArea.text = $"UnityEngine.Screen.safeArea: <b>{UnityEngine.Screen.safeArea.ToString()}";
 
         for (int i = 0; i < UnityEventGroups.Length; i++)
         {
             UnityEventGroups[i].SetActive(Event.value == i);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
         }
     }
 }
